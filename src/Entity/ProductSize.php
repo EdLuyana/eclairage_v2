e7787ebbf1710ac74f6bef35b7386c3540be2e7b
Ed <edouard.duron@lapiscine.pro>
2025-06-18 18:35:56 +0200

    Added new entities to create a better UX

---
+++ src/Entity/ProductSize.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductSizeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProductSizeRepository::class)]
+class ProductSize
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 20)]
+    private ?string $size = null;
+
+    #[ORM\ManyToOne(inversedBy: 'sizes')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Product $product = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSize(): ?string
+    {
+        return $this->size;
+    }
+
+    public function setSize(string $size): static
+    {
+        $this->size = $size;
+        return $this;
+    }
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): static
+    {
+        $this->product = $product;
+        return $this;
+    }
+}
